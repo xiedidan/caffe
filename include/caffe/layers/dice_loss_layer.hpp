5d278fa6255f2c93c64e52c5ff2849c35da2f3d1
xiedidan <xiedidan@yeah.net>
2017-05-16 17:35:34 +0800

    dev commit - dice loss layer forward_cpu() almost done

---
+++ include/caffe/layers/dice_loss_layer.hpp
@@ -0,0 +1,35 @@
+#ifndef CAFFE_DICE_LOSS_LAYER_HPP_
+#define CAFFE_DICE_LOSS_LAYER_HPP_
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+#include "caffe/layers/loss_layer.hpp"
+
+namespace caffe {
+	template <typename Dtype>
+	class DiceWithLossLayer : public LossLayer<Dtype> {
+	public:
+		explicit DiceLossLayer(const LayerParameter& param) : LossLayer<Dtype>(param) {}
+		virtual void Reshape(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top);
+
+		virtual inline const char* type() const { return "DiceLoss"; }
+		virtual inline int ExactNumBottomBlobs() const { return 2; }
+		virtual inline int ExactNumTopBlobs() const { return 1; }
+
+	protected:
+		virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top);
+		virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top);
+		virtual void Backward_cpu(const vector<Blob<Dtype>*>& top, const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+		virtual void Backward_gpu(const vector<Blob<Dtype>*>& top, const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+		vector<Dtype> predictionSum;
+		vector<Dtype> labelSum;
+		vector<Dtype> intersectionSum;
+	};
+} // namespace caffe
+
+#endif  // CAFFE_DICE_LOSS_LAYER_HPP_
---
+++ src/caffe/layers/dice_loss_layer.cpp
@@ -0,0 +1,72 @@
+#include <vector>
+
+#include "caffe/layers/dice_loss_layer.hpp"
+#include "caffe/util/math_functions.hpp"
+
+namespace caffe {
+	template <typename Dtype>
+	void DiceLossLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+		vector<int> topShape(0);
+		top[0]->Reshape(topShape);
+	}
+
+	template <typename Dtype>
+	void DiceLossLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+		const Dtype* data = bottom[0]->cpu_data();
+		const Dtype* label = bottom[1]->cpu_data();
+
+		const int labelCount = bottom[1]->count();
+		const int batchSize = bottom[1]->num();
+		const int dimSize = labelCount / batchSize;
+
+		// bottom[1]->cpu_diff is not used (for backward), so use it to save memory
+		for (int i = 0; i < labelCount; i++) {
+			bottom[1]->mutable_cpu_diff()[i] = data[i] >= data[i + labelCount] ? 0 : 1;
+		}
+		const Dtype* prediction = bottom[1]->cpu_diff();
+
+		top[0]->mutable_cpu_data()[0] = Dtype(0);
+		predictionSum.clear();
+		labelSum.clear();
+		for (int i = 0; i < batchSize; i++) {
+			Dtype currPredictionSum = caffe_cpu_asum(dimSize, prediction + i * dimSize);
+			predictionSum.push_back(currPredictionSum);
+
+			Dtype currLabelSum = caffe_cpu_asum(dimSize, label + i * dimSize);
+			labelSum.push_back(currLabelSum);
+		}
+
+		// again, use bottom[1]->cpu_diff to save memory
+		Dtype* intersection = bottom[1]->mutable_cpu_diff();
+		caffe_mul(labelCount, prediction, label, intersection);
+
+		intersectionSum.clear();
+		for (int i = 0; i < batchSize; i++) {
+			Dtype currIntersectionSum = caffe_cpu_asum(dimSize, intersection + i * dimSize);
+			intersectionSum.push_back(currIntersectionSum);
+
+			top[0]->mutable_cpu_data()[0] += 2.0 * currIntersectionSum / (predictionSum[i] + labelSum[i]);
+		}
+	}
+
+	template <typename Dtype>
+	void DiceLossLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top, const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+		if (propagate_down[1]) {
+			LOG(FATAL) << this->type()
+				<< " Layer cannot backpropagate to label inputs.";
+		}
+
+		if (propagate_down[0]) {
+
+		}
+	}
+
+#ifdef CPU_ONLY
+	STUB_GPU(DiceLossLayer);
+#endif
+	
+	INSTANTIATE_CLASS(EuclideanLossLayer);
+	REGISTER_LAYER_CLASS(EuclideanLoss);
+
+} // namespace caffe
+
